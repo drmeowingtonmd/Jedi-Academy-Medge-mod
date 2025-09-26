e4b6623f7d64e5d791c521e0b59460d7b6a9dec9
drmeowingtonmd <71604947+drmeowingtonmd@users.noreply.github.com>
2025-09-26 11:23:50 -0400

    First commy
    First commit

---
+++ code/speedrun/speedrun_timer_q3/timer.h
@@ -0,0 +1,13 @@
+#pragma once
+
+
+void SpeedrunResetTimer();
+void SpeedrunUpdateTimer();
+void SpeedrunUnpauseTimer(int priority = 0);
+void SpeedrunPauseTimer(int priority = 0);
+void SpeedrunLevelFinished(bool printNotify = false);
+void SpeedrunRunFinished(bool printNotify = true);
+bool SpeedrunIsRunFinished();
+int SpeedrunGetTotalTimeMilliseconds();
+int SpeedrunGetLevelTimeMilliseconds();
+void SpeedrunTimerAddMilliseconds(int msec);
